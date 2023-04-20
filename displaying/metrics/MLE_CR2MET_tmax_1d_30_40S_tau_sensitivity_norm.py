a7282396420fd165dd37bee4d734279b42fed99d
Tomás Carrasco Escaff <tomescaff@gmail.com>
2023-04-20 11:02:10 -0400

    Compare metrics of attribution using barplot

---
+++ displaying/metrics/MLE_CR2MET_tmax_1d_30_40S_tau_sensitivity_norm.py
@@ -0,0 +1,102 @@
+import sys
+import numpy as np
+import xarray as xr
+import pandas as pd
+from scipy.stats import genextreme as gev
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+from os.path import join
+
+sys.path.append('/home/tcarrasco/repo/extreme-weather/processing/')
+
+from utilities import gmst  # noqa: E402
+from utilities import cr2met  # noqa: E402
+from utilities import metrics  # noqa: E402
+
+tglobal_2022 = gmst.annual_5year_smooth().sel(time='2022').values
+tlocal_2023 = cr2met.tmax_1d_djf_30_40S().sel(time='2023').values
+
+
+def tau(yr_list, init_parm):
+    x, y = metrics.obs_data_tmax_1d_remove_year(yr_list)
+    return metrics.tau_best_estimate_norm(x, y, tglobal_2022, tlocal_2023,
+                                          init_parm)
+
+
+init_parms = [[37, 4, 2],
+              [37, 4, 2],
+              [37, 4, 2],
+              [36.5, 0.9, 1.2]]
+
+removed_years = [list(), [2017], [2023], [2017, 2023]]
+
+best_estimates = [tau(years, init_parm)
+                  for years, init_parm in zip(removed_years, init_parms)]
+
+basedir = '/home/tcarrasco/result/data/bootstrap/'
+cr2met = 'MLE_CR2MET_tmax_1d_30_40S_nboot_1000_norm_evaluation.nc'
+no2017 = 'MLE_CR2MET_tmax_1d_30_40S_nboot_1000_norm_no2017_evaluation.nc'
+no2023 = 'MLE_CR2MET_tmax_1d_30_40S_nboot_1000_norm_no2023_evaluation.nc'
+no2017_no2023 = 'MLE_CR2MET_tmax_1d_30_40S_nboot_1000_norm_' + \
+    'no2017_no2023_evaluation.nc'
+
+filenames = [cr2met, no2017, no2023, no2017_no2023]
+models = [xr.open_dataset(join(basedir, filename)) for filename in filenames]
+models = [metrics.bootstrap_tau_norm(model, tglobal_2022, tlocal_2023)
+          for model in models]
+model_names = ['CR2MET', 'No 2017', 'No 2023', 'No 2017 - No2023']
+
+
+def retain_valid(x):
+    # x[~np.isfinite(x)] = 1e10
+    return x
+
+
+tau_arrs = [retain_valid(m['tau'].values) for m in models]
+center = [np.quantile(tau_arr, 0.5, axis=0) for tau_arr in tau_arrs]
+lower = [np.quantile(tau_arr, 0.025, axis=0) for tau_arr in tau_arrs]
+upper = [np.quantile(tau_arr, 0.975, axis=0) for tau_arr in tau_arrs]
+
+# Add every font at the specified location
+font_dir = ['/home/tcarrasco/result/fonts/Merriweather',
+            '/home/tcarrasco/result/fonts/arial']
+for font in font_manager.findSystemFonts(font_dir):
+    font_manager.fontManager.addfont(font)
+
+plt.rcParams['font.family'] = 'arial'
+plt.rcParams['font.size'] = 10
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+
+fig, ax = plt.subplots(1, 1, figsize=(12, 3))
+varname = 'tau'
+
+width = np.array(upper) - np.array(lower)
+y_pos = np.arange(len(model_names))
+barlist = ax.barh(y_pos, width=width, left=lower,
+                  height=0.4, align='center')
+colors = ['#3EC1D3', '#3EC1D3', '#3EC1D3', '#3EC1D3']
+for bar, color in zip(barlist, colors):
+    bar.set_color(color)
+plt.scatter(best_estimates, y_pos, s=200, marker='|', color=[
+            'k', 'k', 'k', 'k'], zorder=4)
+plt.yticks(y_pos, model_names)
+ax.set_axisbelow(True)
+plt.grid(lw=0.4, ls='--', color='grey', zorder=-4)
+plt.ylim([-0.5, 3.5])
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Actual return period')
+ax.spines.right.set_visible(False)
+ax.spines.left.set_visible(False)
+ax.spines.top.set_visible(False)
+ax.tick_params(direction="in")
+ax.set_xscale('log')
+plt.xlim([0.1, 1e9])
+plt.xticks([0.1, 1.0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9])
+plt.axvline(1, color='k', lw=1.0)
+plt.tight_layout()
+basedir = '/home/tcarrasco/result/images/png/'
+filename = 'MLE_tmax_1d_metrics_sensitivity_norm.png'
+filepath = basedir + filename
+plt.savefig(filepath, dpi=300)
+# plt.show()
---
+++ displaying/metrics/MLE_tmax_1d_30_40S_future_gev.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+from os.path import join
+
+basedir = '/home/tcarrasco/result/data/all_estimate/'
+
+lens1 = 'MLE_LENS1_tmax_1d_30_40S_all_estimate_nboot_100_gev_evaluation.csv'
+
+filenames = [lens1, lens1, lens1, lens1, lens1, lens1, lens1, lens1]
+models = [pd.read_csv(join(basedir, filename), index_col=0)
+          for filename in filenames]
+model_names = ['CESM1-LENS Method #1',
+               'CESM1-LENS Method #2',
+               'CESM2-LENS Method #1',
+               'CESM2-LENS Method #2',
+               'EC Earth Method #1',
+               'EC Earth Method #2',
+               'Model z Method #1',
+               'Model z Method #2']
+
+# Add every font at the specified location
+font_dir = ['/home/tcarrasco/result/fonts/Merriweather',
+            '/home/tcarrasco/result/fonts/arial']
+for font in font_manager.findSystemFonts(font_dir):
+    font_manager.fontManager.addfont(font)
+
+plt.rcParams['font.family'] = 'arial'
+plt.rcParams['font.size'] = 10
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+
+fig, axs = plt.subplots(4, 1, figsize=(8, 10))
+
+varnames = ['tau_ac', 'tau_fu', 'PR fu-ac', 'Delta fu-ac']
+xlabels = ['Factual return period (yr)', 'Future return period (yr)',
+           'Future probability ratio', 'Future intesity change (ºC)']
+xlims = [[0.1, 10000], [0.1, 10000], [0.1, 10000], [-1.5, 1.5]]
+for varname, ax, xlim, xlabel in zip(varnames, axs, xlims, xlabels):
+    center = [m.loc['Best estimate', varname] for m in models]
+    lower = [m.loc['Upper estimate', varname] for m in models]
+    upper = [m.loc['Lower estimate', varname] for m in models]
+    width = np.array(upper) - np.array(lower)
+
+    plt.sca(ax)
+
+    y_pos = np.arange(len(model_names))
+    barlist = ax.barh(y_pos, width=width, left=lower,
+                      height=0.4, align='center')
+    colors = ['#3EC1D3', '#3EC1D3', 'none',
+              'none', 'none', 'none', 'none', 'none']
+    for bar, color in zip(barlist, colors):
+        bar.set_color(color)
+    ax.scatter(center, y_pos, s=80, marker='|', color=[
+        'k', 'k', 'none', 'none', 'none', 'none', 'none', 'none'], zorder=4)
+    ax.set_yticks(y_pos, model_names)
+    ax.set_xlim(xlim)
+    ax.set_xlabel(xlabel)
+    ax.set_axisbelow(True)
+    ax.grid(lw=0.4, ls='--', color='grey', zorder=-4)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel(xlabel)
+    # ax.tick_params(direction="in")
+
+    if varname != 'Delta fu-ac':
+        # xticks = np.arange(1, 11, 1)
+        # xticks = np.append(xticks, [xticks*z for z in [1e1, 1e2, 1e3, 1e4]])
+        ax.set_xscale('log')
+        # ax.set_xticks(xticks)
+
+plt.tight_layout()
+basedir = '/home/tcarrasco/result/images/png/'
+filename = 'MLE_tmax_1d_metrics_future_nboot_100.png'
+filepath = basedir + filename
+plt.savefig(filepath, dpi=300)
+# plt.show()
